9369b51c0fc2ac09a4320fa0f97b468f06ee4834
codingMaz <m.balandari.toroghi@gmail.com>
2025-06-29 14:38:21 +0330

    update the contact view for form

--- dental/views.py
+++ dental/views.py
@@ -4,4 +4,11 @@ def home(request):
     return render(request, "home.html")
 
 def contact(request):
-    return render(request, "contact.html")
+    if request.method == "POST":
+        message_name = request.POST["message-name"]
+        message_email = request.POST["message-email"]
+        message = request.POST["message"]
+        return render(request, "contact.html", {"message_name": message_name,})
+
+    else:
+        return render(request, "contact.html")
