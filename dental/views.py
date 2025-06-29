267c7b30433611cf50580d17adf56b1eb91584af
codingMaz <m.balandari.toroghi@gmail.com>
2025-06-29 14:56:58 +0330

    add gmail email sender

--- dental/views.py
+++ dental/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, "home.html")
@@ -8,7 +9,10 @@ def contact(request):
         message_name = request.POST["message-name"]
         message_email = request.POST["message-email"]
         message = request.POST["message"]
-        return render(request, "contact.html", {"message_name": message_name,})
+
+        send_mail(message_name, message, message_email, ["info@dento.com"], fail_silently=True)
+
+        return render(request, "contact.html", {"message_name": message_name})
 
     else:
         return render(request, "contact.html")
--- project_django/settings.py
+++ project_django/settings.py
@@ -130,3 +130,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "info.dento@gmail.com"
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
